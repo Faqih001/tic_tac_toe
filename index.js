44ee93b36954ab720fcd4efbb340c9476060e16a
Amirul Faqih <fakiiahmad001@gmail.com>
2024-07-07 23:08:19 +0300

    chore: register the app component with the AppRegistry

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
+// register the app component with the AppRegistry and the name of the app as appName
 AppRegistry.registerComponent(appName, () => App);
